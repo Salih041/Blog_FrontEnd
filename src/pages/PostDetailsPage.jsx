c50eebbd4e9e72059dba056153dbb13ccc8862af
Salih041 <salihozbk41@gmail.com>
2025-11-20 00:15:30 +0300

    Update PostDetail css

--- src/pages/PostDetailsPage.jsx
+++ src/pages/PostDetailsPage.jsx
@@ -4,6 +4,8 @@ import { useParams, NavLink, useNavigate } from 'react-router-dom'
 import api from "../api"
 import { useAuth } from '../context/AuthContext'
 
+import "../styles/PostDetail.css"
+
 function PostDetailsPage() {
 
   const [post, setPost] = useState(null);
@@ -96,6 +98,35 @@ function PostDetailsPage() {
     }
   }
 
+  const handleLikeComment = async (comment_id) => {
+    if (!isLoggedIn) {
+      alert("Please LOGIN")
+      return
+    }
+    try {
+      const response = await api.put(`/posts/${id}/comment/${comment_id}/like`);
+      const { likeCount, likes } = response.data;
+      setPost(prevPost => {
+        const updatedComments = prevPost.comments.map(comment => {
+          if (comment._id === comment_id) {
+            return {
+              ...comment,
+              likeCount: likeCount,
+              likes: likes
+            }
+          }
+          return comment;
+        });
+        return {
+          ...prevPost,
+          comments : updatedComments
+        }
+      });
+    } catch (error) {
+      console.error("Like error : ", error);
+    }
+  }
+
   if (isLoading) return (<p>Loading</p>)
   if (error) return (<p>Error: {error}</p>)
   if (!post) return (<h4>Post Not Found</h4>)
@@ -103,60 +134,79 @@ function PostDetailsPage() {
   const hasLiked = post.likes.includes(userId);
   const isOwner = isLoggedIn && userId === post.author._id;
   return (
-    <div>
-      {isOwner && (
-        <div className="post-controls" style={{ backgroundColor: '#fff8e1', padding: '10px' }}>
-          <strong>Author Panel:</strong>
-          <button onClick={handleDeletePost} style={{ marginLeft: '10px', color: 'red' }}>
-            Delete Post
-          </button>
-          <NavLink to={`/posts/edit/${post._id}`} style={{ marginLeft: '10px' }}>
-            Edit Post
-          </NavLink>
-        </div>
-      )}
-      <h1>{post.title}</h1>
-      <p>{post.author.username}</p>
-      <p>{post.content}</p>
-      <footer>
-        <p>
-          <button onClick={handleLike} disabled={!isLoggedIn}>
-            {/* Artık dinamik bir metin göster */}
-            {hasLiked ? '🌧️ Undo' : 'Like'}
-          </button>
-          {post.likeCount} Like
-        </p>
-      </footer>
-      <p>⛈️{post.commentCount}</p>
-      {isLoggedIn ? (
-        <form onSubmit={handleCommentSubmit}>
-          <textarea
-            rows="3"
-            placeholder="Write..."
-            value={commentText}
-            onChange={(e) => setCommentText(e.target.value)}
-            required
-          />
-          <button type="submit" disabled={isSubmitting}>
-            {isSubmitting ? '...' : 'Submit'}
-          </button>
-        </form>
-      ) : (
-        <p>Login<NavLink to="/login">to comment⛈️</NavLink>.</p>
-      )}
-      <div className="comment-list">
-        {post.comments.length > 0 ? (
-          post.comments.toReversed().map(comment => (
-            <article key={comment._id} className="comment-bubble">
-              <p><strong>{comment.author.username}</strong></p>
-              <p>{comment.text}</p>
-            </article>
-          ))
-        ) : (
-          <p>Henüz yorum yapılmamış.</p>
+    <div className='Post-detail-wrapper'>
+      <article className='full-post-artice'>
+        {isOwner && (
+          <div className='owner-controls'>
+            <span>Author Panel:</span>
+            <NavLink className="link-edit" to={`/posts/edit/${post._id}`}>Edit Post</NavLink>
+            <button className='button-delete' onClick={handleDeletePost}>Delete Post</button>
+          </div>
         )}
-      </div>
-    </div>
+
+        <header className='post-header'>
+          <h1 className='post-title'>{post.title}</h1>
+          <div className='post-meta'>
+            <span>by <NavLink to={`/profil/${post.author._id}`}>{post.author.username}</NavLink></span>
+            <span>•</span>
+            <span>📅 {new Date(post.createdAt).toLocaleDateString('tr-TR')}</span>
+          </div>
+        </header>
+
+        <div className='post-content'>
+          {post.content}
+        </div>
+
+        <footer className='post-footer'>
+          <button className='like-button' onClick={handleLike} disabled={!isLoggedIn} style={{ color: hasLiked ? '#e74c3c' : '', borderColor: hasLiked ? '#e74c3c' : '' }}>
+            {hasLiked ? '❤️ Liked' : '🤍 Like'}
+          </button>
+          <span><strong>{post.likeCount}</strong>Likes</span>
+        </footer>
+      </article>
+
+      <section className='comment-section'>
+        <h2>Comments {post.commentCount}</h2>
+
+        {isLoggedIn ? (
+          <form onSubmit={handleCommentSubmit} className='comment-form'>
+            <textarea rows="3" placeholder='Write your comments' value={commentText} onChange={(e) => { setCommentText(e.target.value) }} required ></textarea>
+            <button type='submit' disabled={isSubmitting}>{isSubmitting ? '...' : 'Comment'}</button>
+          </form>
+        ) : (
+          <p style={{ marginBottom: '20px' }}>
+            <Link to="/login" style={{ color: '#3498db' }}>Login</Link> to comment.
+          </p>)}
+
+        <div className='comment-list'>
+          {
+            post.commentCount > 0 ? (
+              post.comments.toReversed().map(comment => {
+                const hasLikedComment = comment.likes.includes(userId);
+                return (
+                  <article key={comment._id} className='comment-bubble'>
+                    <strong className="comment-author"><NavLink to={`/profil/${comment.author._id}`}>
+                    {comment.author.username}</NavLink></strong>
+                    <p className="comment-text">{comment.text}</p>
+                    <div className="comment-actions">
+                      <button
+                        className="comment-like-btn"
+                        onClick={() => handleLikeComment(comment._id)}
+                        disabled={!isLoggedIn}
+                      >
+                        {hasLikedComment ? '❤️' : '🤍'} Like
+                      </button>
+                      <span>{comment.likeCount}</span>
+                      <span style={{ marginLeft: '10px' }}>• {new Date(comment.createdAt).toLocaleDateString()}</span>
+                    </div>
+                  </article>
+                )
+              })
+            ) : (<p style={{ color: '#777', fontStyle: 'italic' }}>There are no comment</p>)
+          }
+        </div>
+      </section>
+    </div >
   )
 }
 
